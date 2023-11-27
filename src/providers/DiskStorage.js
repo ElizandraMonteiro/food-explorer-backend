1ce67c4a7c3942a6ac0f98a776b5a1740c1a39cd
Elizandra Monteiro <monteiroelizandra2017@gmail.com>
2023-11-27 08:55:29 -0300

    Changes

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -1,33 +1,28 @@
-// FS, Path and UploadConfig Import
 const fs = require('fs');
 const path = require('path');
 const uploadConfig = require('../configs/upload');
 
 class DiskStorage {
-    async saveFile(file){
+    async saveFile(file) {
         await fs.promises.rename(
-            // Initial position file
             path.resolve(uploadConfig.TMP_FOLDER, file),
-            // Final position file
             path.resolve(uploadConfig.UPLOADS_FOLDER, file)
         );
 
         return file;
     }
 
-    async deleteFile(file){
-        // Getting file address
+    async deleteFile(file) {
+
         const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
 
         try {
-            // If exists
+
             await fs.promises.stat(filePath);
         } catch {
-            // Stopping
             return;
         }
 
-        // Deleting
         await fs.promises.unlink(filePath);
     }
 }
